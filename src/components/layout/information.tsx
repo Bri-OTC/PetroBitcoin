ca79b363d7ffeeb73061dbabde0259c045d06f77
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2024-07-04 17:52:22 +0300

    maintenance

--- src/components/layout/information.tsx
+++ src/components/layout/information.tsx
@@ -9,19 +9,18 @@ export default function Information() {
   return (
     <>
       {openInfoMenu && (
-        <div className="bg-transparent fixed top-0 left-0 right-0 z-[50] w-full">
-          <div className="lg:max-w-[1280px] mx-auto px-[15px]">
+        <div className="bg-[#D7C04C] py-[8px] fixed top-0 left-0 right-0 z-[50] w-full">
+          <div className="lg:max-w-[1280px] mx-[auto] px-[15px]">
             <div className="relative">
               <div className="flex items-center justify-center">
                 <VelocityScroll
-                  /*
                   text=" Upgrading Testnet Intents Systems | Resuming Shortly | Collaterals are not Affected | Positions can be managed from explorer |            "
                   default_velocity={5}
                   className=" text-center text-2xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white "
-                  */
+                  /*
                   text=" Testnet    |       "
                   default_velocity={10}
-                  className="text-xs tracking-[-0.02em] text-black drop-shadow-sm dark:text-white "
+                  className="text-xs tracking-[-0.02em] text-black drop-shadow-sm dark:text-white "*/
                 />
               </div>
             </div>
